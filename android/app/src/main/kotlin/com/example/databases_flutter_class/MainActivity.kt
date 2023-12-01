9ee3e02b1010dfda0a8ea5a3c4fc2e5fdb019322
https://github.com/sunnyrai87/custom_widget.git <sunnyraironey@gmail.com>
2023-12-01 18:25:12 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/databases_flutter_class/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.databases_flutter_class
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:databases_flutter_class/app_db.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Notes"),
+        ),
+        body: Container(),
+        floatingActionButton:
+            FloatingActionButton(onPressed: () {
+              var appDB = AppDataBase.instance;
+              appDB.addNote("New Note", "Implement DB in flutter app");
+
+            }, child: Icon(Icons.add)));
+  }
+}
