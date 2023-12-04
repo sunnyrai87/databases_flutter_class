a93d0a9f4c06a8a487df8057c88aa34e9e8ec1b1
https://github.com/sunnyrai87/custom_widget.git <sunnyraironey@gmail.com>
2023-12-04 12:53:10 +0530

    today

---
+++ lib/2_pair_25-Nov-2023/main2.dart
@@ -0,0 +1,76 @@
+//
+// import 'package:flutter/material.dart';
+//
+// import 'app_db.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const HomePage(),
+//     );
+//   }
+// }
+//
+// class HomePage extends StatefulWidget {
+//   const HomePage({super.key});
+//
+//   @override
+//   State<HomePage> createState() => _HomePageState();
+// }
+//
+// class _HomePageState extends State<HomePage> {
+//
+//   late AppDataBase appDB;
+//   List<Map<String, dynamic>> data =[];
+//   @override
+//   void initState() {
+//
+//     super.initState();
+//     appDB = AppDataBase.instance;
+//     getAllNotes();
+//   }
+//
+//   void getAllNotes() async{
+//
+//     data = await appDB.fetchNotes();
+//     setState(() {
+//
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: Text("Notes"),
+//         ),
+//         body: ListView.builder(
+//             itemCount: data.length,
+//             itemBuilder: (context, index) {
+//               return ListTile(
+//                 title: Text('${data[index]['title']}'),
+//                 subtitle: Text('${data[index]['desc']}'),
+//               );
+//             }),
+//         floatingActionButton:
+//             FloatingActionButton(onPressed: () {
+//               var appDB = AppDataBase.instance;
+//               appDB.addNote("New Note", "Implement DB in flutter app");
+//               getAllNotes();
+//
+//             }, child: Icon(Icons.add)));
+//   }
+// }
---
+++ lib/3_pair_27-Nov-2023/note_model.dart
@@ -0,0 +1,21 @@
+// import 'package:databases_flutter_class/3_pair_27-Nov-2023/app_db.dart';
+//
+// class NoteModel  {
+//   int note_id;
+//   String note_title;
+//   String note_desc;
+//
+//   NoteModel({required this.note_id,
+//     required this.note_title,
+//     required this.note_desc});
+//
+//
+// //fromMap --> Model
+// factory NoteModel.fromMap(Map<String, dynamic> map){
+//   return NoteModel(note_id:map[AppDataBase.COLUMN_NOTE_ID],
+//   note_title: map[AppDataBase.COLUMN_NOTE_TITLE],
+//   note_desc :  map[AppDataBase.COLUMN_NOTE_DESC],
+//   );}}
+//
+//
+// // Model --> toMap
